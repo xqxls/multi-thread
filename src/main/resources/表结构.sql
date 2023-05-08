7779854d8a8b1d613fc3e58a83127c9574ad9e24
xqxls <1078774751@qq.com>
2023-05-08 20:31:46 +0800

    1.修改表结构 2.跟新README文档

--- src/main/resources/表结构.sql
+++ src/main/resources/表结构.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `batch_in_table` (
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-  `str` varchar(64) DEFAULT NULL COMMENT '插入内容',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='批量插入表';
+    `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+    `str` varchar(64) DEFAULT NULL COMMENT '插入内容',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='批量插入表';
 
 CREATE TABLE `batch_in_table2` (
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-  `str` varchar(64) DEFAULT NULL COMMENT '插入内容',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='批量插入表2';
+     `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+     `str` varchar(64) DEFAULT NULL COMMENT '插入内容',
+     PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='批量插入表2';
